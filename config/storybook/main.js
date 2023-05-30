67e85e2eec46435514969758089ad3096d117a48
OnishchenkoIvan <iv.onishchenko@mail.ru>
2023-05-30 21:56:57 +0300

    upgrade sidebar, add button themes

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,15 +1,25 @@
-module.exports = {
+const config = {
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: { fastRefresh: true },
+  },
   stories: ["../../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  features: {
+    storyStoreV7: false,
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: { fastRefresh: true },
-  },
   core: {
-    builder: "webpack5",
+    builder: {
+      name: "@storybook/builder-webpack5",
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
   },
 };
+export default config;
